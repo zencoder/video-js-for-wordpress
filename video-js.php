730480dcc36dda1a8314956172d36c881e9936f2
unknown <Dustin@Dustin-PC.(none)>
2012-09-20 20:20:54 -0600

    Changed version to 3.2.1 (video.js 3.2.2 isn't out yet)

--- video-js.php
+++ video-js.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Video.js
- * @version 3.2.2
+ * @version 3.2.1
  */
 /*
 Plugin Name: Video.js - HTML5 Video Player for WordPress
 Plugin URI: http://videojs.com/
 Description: A video plugin for WordPress built on the widely used Video.js HTML5 video player library. Allows you to embed video in your post or page using HTML5 with Flash fallback support for non-HTML5 browsers.
 Author: <a href="http://steveheffernan.com">Steve Heffernan</a>, <a href="http://www.nosecreekweb.ca">Dustin Lammiman</a>
-Version: 3.2.2
+Version: 3.2.1
 */
 
 
